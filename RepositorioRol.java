3ea92056123c058819036c4dddd776c79e65ff3a
jhon320 <125573414+jhon320@users.noreply.github.com>
2023-05-09 22:33:36 -0500

    eliminar repsitorio

--- RepositorioRol.java
+++
@@ -1,7 +0,0 @@
-package com.registraduria.seguridad.Repositorios;
-
-import com.registraduria.seguridad.Modelos.Rol;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface RepositorioRol extends MongoRepository<Rol,String> {
-}
